0d04708db7749d917b2e026a2e91b41b2a7feebb
Vinay Linginedi <43011798+vinaylinginedi@users.noreply.github.com>
2019-09-12 23:51:57 -0500

    Add date picker functionality

---
+++ instructor_dashboard.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+?>
+
+<html>
+<head>
+<title>Instructor Dashboard</title>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+
+<script>
+$(function() {
+  $('input[name="daterange"]').daterangepicker({
+    opens: 'left'
+  }, function(start, end, label) {
+    console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+  });
+});
+</script>
+</head>
+
+<body>
+</body>
+</html>
