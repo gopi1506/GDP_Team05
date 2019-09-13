0caa268c02ea23a3bc1718a249174798da55fe86
Vinay Linginedi <43011798+vinaylinginedi@users.noreply.github.com>
2019-09-13 00:50:07 -0500

    Create tab selection design

--- instructor_dashboard.php
+++ instructor_dashboard.php
@@ -22,5 +22,20 @@ $(function() {
 </head>
 
 <body>
+<div class='header'>
+<h2 class ='name'>CODEWORD</h2>
+<a href='logout.php' style='text-decoration:none;'><h2 class ='name' style='float:right;'>Logout</h2></a>
+</div>
+
+<div class='body'>
+<h1 style='color:#00000;padding-left:530px; '>Instructor Dashboard</h1>
+
+<div class='body_main'>
+<div class='tab_bar'>
+<a href='#' style='text-decoration:none;'><h3 class='tab_name active' >Add Course</h3></a>
+<a href='instructor_dashboard_course.php' style='text-decoration:none;'><h3 class='tab_name'>Course</h3></a>
+<a href='instructor_dashboard_codeword.php' style='text-decoration:none;'><h3 class='tab_name'>Codeword</h3></a>
+</div>
+</div>
 </body>
 </html>
