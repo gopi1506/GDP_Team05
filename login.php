c0fe83f2453a450fb1f3546165fb660f89d25f6f
Harika Naidu <nagaharika.n@gmail.com>
2019-09-13 11:13:48 -0500

    Add login layout field in login page

--- login.php
+++ login.php
@@ -21,3 +21,14 @@ session_start();
 <h2 style='color:#fff;padding-top:10px;'>LOGIN</h2>
 </div>
 <div class='mini-body'>
+
+<form action='login_form.php' method='post'>
+<input type='email' placeholder='Email' name='email' class='form_input' required/><br>
+<input type='password' placeholder='Password' name='password' class='form_input' required/><br>
+<input type='submit' value='LogIn' style='background:#59af50;color:#fff;width:30%;height:15%;margin:6px 0px 6px 0px;'/>
+</form>
+<a href='#' style='text-decoration:none;float:left;'><h4 style='color:#000;margin:0px;padding-left:20px;'>Forgot Password?</h4></a>
+
+<a href='index.php' style='text-decoration:none;float:right;'><h4 style='color:#000;margin:0px;padding-right:20px;'>Don't have an account?SignUp</h4></a>
+
+</div>
