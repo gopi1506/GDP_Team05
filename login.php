c884f66a2a89154eb226ff0d215c4750d81ff4c1
Harika Naidu <nagaharika.n@gmail.com>
2019-09-13 01:50:31 -0500

    Add style to login page title

--- login.php
+++ login.php
@@ -13,3 +13,11 @@ session_start();
 <div class='header'>
 <h2 id='name'>CODEWORD</h2>
 </div>
+<div class='body'>
+
+<center><div class='signup'>
+
+<div class='mini-header'>
+<h2 style='color:#fff;padding-top:10px;'>LOGIN</h2>
+</div>
+<div class='mini-body'>
