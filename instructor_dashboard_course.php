3fdb2b821bed0d3a9f4e03d335c0966edb65233c
Gopi Krishna Amara <43010422+gopi1506@users.noreply.github.com>
2019-09-13 00:52:08 -0500

    Create instructor_dashboard_course.php

---
+++ instructor_dashboard_course.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+?>
+
+<html>
+<head>
+<title>Instructor Dashboard</title>
+</head>
+
+<body>
+<div class='header'>
+<h2 class ='name'>CODEWORD</h2>
+<a href='logout.php' style='text-decoration:none;'><h2 class ='name' style='float:right;'>Logout</h2></a>
+
+</div>
+<div class='body'>
+<h1 style='color:#9d9d9d;padding-left:22px; '>Instructor Dashboard</h1>
+
+<div class='body_main'>
+<div class='tab_bar'>
+<a href='instructor_dashboard.php' style='text-decoration:none;'><h3 class='tab_name'>Add Course</h3></a>
+<a href='#' style='text-decoration:none;'><h3 class='tab_name active'>Course</h3></a>
+<a href='instructor_dashboard_codeword.php' style='text-decoration:none;'><h3 class='tab_name'>Codeword</h3></a>
+</div>
+<div class='card_view_bar'></div>
+</div>
+</div>
+</body>
+</html>
