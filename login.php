b05aca7dc7f122a45854102bfa4a63da7f2bb13c
Vinay Linginedi <43011798+vinaylinginedi@users.noreply.github.com>
2019-09-16 09:19:53 -0500

    Add Unique styling to maintain consistency

--- login.php
+++ login.php
@@ -13,17 +13,17 @@ session_start();
             .header{
                 width:100%;
                 height:10%;
-                background:#59af50;
+                background:#b3b3b3;
             }
             .body{
                 width:100%;
                 height:90%;
-                background:#aed581;
+                background:#4d4d4d;
             }
             #name{
                 margin:0px;
                 padding:22px 0px 0px 10px;
-                color:#fff;
+                color:#000000;
             }
             .signup{
                 width:50%;
@@ -33,12 +33,12 @@ session_start();
             .mini-header{
                 width:100%;
                 height:20%;
-                background:#59af50;
+                background:#cccccc;
             }
             .mini-body{
                 width:100%;
                 height:80%;
-                background:#dcedc9;
+                background:#808080;
             }
             .form_input{
                 width:80%;
@@ -58,13 +58,13 @@ session_start();
             <center>
                 <div class='signup'>
                     <div class='mini-header'>
-                        <h2 style='color:#fff;padding-top:10px;'>LOGIN</h2>
+                        <h2 style='color:#000000;padding-top:10px;'>LOGIN</h2>
                     </div>
                     <div class='mini-body'>
                         <form action='login_form.php' method='post'>
                             <input type='email' placeholder='Email' name='email' class='form_input' required/><br>
                             <input type='password' placeholder='Password' name='password' class='form_input' required/><br>
-                            <input type='submit' value='LogIn' style='background:#59af50;color:#fff;width:30%;height:15%;margin:6px 0px 6px 0px;'/>
+                            <input type='submit' value='LogIn' style='background:#000000;color:#fff;width:30%;height:15%;margin:6px 0px 6px 0px;'/>
                         </form>
                         <a href='#' style='text-decoration:none;float:left;'><h4 style='color:#000;margin:0px;padding-left:20px;'>Forgot Password?</h4></a>
                         <a href='index.php' style='text-decoration:none;float:right;'><h4 style='color:#000;margin:0px;padding-right:20px;'>Don't have an account?SignUp</h4></a>
