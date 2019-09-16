ba5bb3c6d7fa3d25261828532e4dbd7fe8b87b74
Vinay Linginedi <43011798+vinaylinginedi@users.noreply.github.com>
2019-09-16 09:38:18 -0500

    Create first view in codeword

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+?>
+
+<html>
+<head>
+<title>Signup</title>
+</head>
+<body>
+
+<div class='header'>
+<h2 id='name'>CODEWORD</h2>
+</div>
+<div class='body'>
+<center><div class='signup'>
+<div class='mini-header'>
+<h2 style='color:#000000;padding-top:5px;'>SIGN UP</h2>
+</div>
+<div class='mini-body'>
+<form action='signup_form.php' method='post'>
+*****
+</form>
+
+<a href='login.php' style='text-decoration:none;float:right;'><h4 style='color:#000;margin:0px;'>Already have an account?LogIn</h4></a>
+</div>
+</div></center>
+</div>
+</body>
+</html>
