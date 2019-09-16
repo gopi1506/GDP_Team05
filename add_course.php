4808de9ff84a69c74fcf6079ac7f295c0db74b31
Gopi Krishna Amara <43010422+gopi1506@users.noreply.github.com>
2019-09-16 09:11:41 -0500

    update add_course.php
    Add if condition to check if the course name is already exists

--- add_course.php
+++ add_course.php
@@ -2,4 +2,15 @@
 session_start();
 ?>
 <?php
+
+   if ( isset($_FILES["file"])) {
+        if ($_FILES["file"]["error"] > 0) {
+            echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
+
+        }
+           
+     } else {
+             echo "No file selected <br />";
+     }
+
 ?>
