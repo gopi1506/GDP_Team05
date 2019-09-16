6c27f3bd78a353aaa33e243e5f33f0d7323bf285
Vinay Linginedi <43011798+vinaylinginedi@users.noreply.github.com>
2019-09-16 09:25:44 -0500

    Add styling to codeword tab in instructor dashboard

--- instructor_dashboard_codeword.php
+++ instructor_dashboard_codeword.php
@@ -5,6 +5,57 @@ session_start();
 <html>
 <head>
 <title>Instructor Dashboard</title>
+<style>
+body{
+margin:0px;
+padding:0px;
+}
+.header{
+width:100%;
+height:10%;
+background:#b3b3b3;
+}
+.name{
+margin:0px;
+padding:20px 20px;
+color:#000000;
+float:left;
+}
+.body{
+width:100%;
+height:80%;
+}
+.body_main{
+width:100%;
+height:80%;
+background:#4d4d4d;
+margin:0px auto;
+}
+.tab_bar{
+width:100%;
+height:10%;
+}
+
+.tab_name{
+width:25%;
+height:100%;
+color:#cccccc;
+float:left;
+text-align:center;
+margin:0px;
+padding-top:10px;
+}
+
+.tab_name:hover{
+background:#cccccc;
+transition:1s ease;
+}
+.active{
+background:#cccccc;
+color:#000000;
+}
+
+</style>
 </head>
 <body>
 <div class='header'>
