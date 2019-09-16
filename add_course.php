660d13f60658fa559dca2b2e0897922517bf9690
Gopi Krishna Amara <43010422+gopi1506@users.noreply.github.com>
2019-09-16 09:29:15 -0500

    Update add_course.php
    check the file status and store the file in the directory "upload"

--- add_course.php
+++ add_course.php
@@ -8,6 +8,24 @@ session_start();
             echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
 
         }
+         else {
+                 //Print file details
+             echo "Upload: " . $_FILES["file"]["name"] . "<br />";
+             echo "Type: " . $_FILES["file"]["type"] . "<br />";
+             echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
+             echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br />";
+
+                 //if file already exists
+             if (file_exists("student/" . $_FILES["file"]["name"])) {
+            echo $_FILES["file"]["name"] . " already exists. ";
+             }
+             else {
+                    //Store file in directory "upload" with the name of "uploaded_file.txt"
+            $storagename = "student01.txt";
+            move_uploaded_file($_FILES["file"]["tmp_name"], "student/" . $storagename);
+            echo "Stored in: " . "student/" . $_FILES["file"]["name"] . "<br />";
+            }
+        }
            
      } else {
              echo "No file selected <br />";
