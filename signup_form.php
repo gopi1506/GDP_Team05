770284bb74d8d90429386db3eb190f0498b97fc6
Harika Naidu <nagaharika.n@gmail.com>
2019-09-16 23:05:29 -0500

    Add signup page and fields to enter

---
+++ signup_form.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+?>
+
+<html>
+    <head>
+        <title>User Registaration</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="header">
+                <h2>Register</h2>
+                <form action="signup_form.php" method="post">
+                    <div>
+                        <label for= "username">User Name : </label>
+                        <input type="text" name="username">
+                    </div>
+                    <div>
+                        <label for= "email">Email : </label>
+                        <input type="email" name="email">
+                    </div>
+                    <div>
+                        <label for= "password">Password : </label>
+                        <input type="password" name="password_1">
+                    </div>
+                    <div>
+                        <label for= "password">Confirm Password : </label>
+                        <input type="password" name="password_2">
+                    </div>
+                    <button type="submit" name="reg_user">Submit</button>
+                    <p>Already have an account<a href="login.php"><b>Log in</b></a></p>
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
