d29fa652fbaf0067d56feb5cff939398924b556b
Harika Naidu <nagaharika.n@gmail.com>
2019-09-16 21:48:28 -0500

    Add heading to admin page

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -3,6 +3,8 @@
     <title>Admin Dashboard</title>
     </head>
     <body>
-        <div>CODEWORD</div>
+        <div class='header'>
+            <h2 class='name'>CODEWORD</h2>
+        </div>
     </body>
 </html>
