01bf8a480916b94d041bd31e1086afbb35f1c6bc
Harika Naidu <nagaharika.n@gmail.com>
2019-09-16 21:56:59 -0500

    Add styling to header in admin page

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -1,6 +1,19 @@
 <html>
     <head>
     <title>Admin Dashboard</title>
+    <style>
+        .header{
+            width:100%;
+            height:10%;
+            background:#b3b3b3
+        }
+        .name{
+            margin:0px;
+            padding:20px 20px;
+            color:#000000;
+            float:left;
+        }
+    </style>
     </head>
     <body>
         <div class='header'>
