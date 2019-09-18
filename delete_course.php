20f6709e145cd6b39a24cb13a0e748550045e488
Gopi Krishna Amara <43010422+gopi1506@users.noreply.github.com>
2019-09-17 19:01:26 -0500

    Create delete_course.php
    Add code for deleting a course base on course code

---
+++ delete_course.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+?>
+<?php
+error_reporting(0);
+
+$course_code = $_POST['course_code'];
+
+$db = mysqli_connect('localhost', 'root', '', 'gdp') or die('error connecting to mysql db');
+$query = "drop table ".$course_code."";
+$query1 = "delete from course where course_code='$course_code'";
+
+$query2 = mysqli_query($db, $query) or die('error querying db');
+$query33 = mysqli_query($db, $query1) or die('error querying db');
+
+
+include('instructor_dashboard_course.php');
+echo " <script>alert('Sucesssfully Deleted Course');</script>  ";
+
+
+?>
