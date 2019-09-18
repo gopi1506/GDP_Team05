37416cd7baeb2e9aa81ba29de6617e931ffa8b87
Gopi Krishna Amara <43010422+gopi1506@users.noreply.github.com>
2019-09-17 19:18:51 -0500

    Update index.php
    Modify code for form input

--- index.php
+++ index.php
@@ -1,29 +1,106 @@
 <?php
 session_start();
 ?>
-
+<?php
+error_reporting(0);
+?>
 <html>
 <head>
 <title>Signup</title>
+<style>
+body{
+margin:0px;
+padding:0px;
+}
+.header{
+width:100%;
+height:10%;
+background:#59af50;
+}
+.body{
+width:100%;
+height:90%;
+background:#aed581;
+}
+
+#name{
+margin:0px;
+padding:22px 0px 0px 10px;
+color:#fff;
+}
+.signup{
+width:50%;
+height:60%;
+padding-top:30px;
+}
+.mini-header{
+width:100%;
+height:10%;
+background:#59af50;
+}
+.mini-body{
+width:100%;
+height:90%;
+background:#dcedc9;
+}
+.form_input{
+width:80%;
+height:8%;
+border:0px;
+margin:8px 0px 8px 0px;
+padding-left:4px;
+
+}
+
+
+
+</style>
 </head>
 <body>
 
 <div class='header'>
 <h2 id='name'>CODEWORD</h2>
 </div>
+
+
 <div class='body'>
+
 <center><div class='signup'>
+
 <div class='mini-header'>
-<h2 style='color:#000000;padding-top:5px;'>SIGN UP</h2>
+<h2 style='color:#fff;padding-top:10px;'>SIGN UP</h2>
 </div>
 <div class='mini-body'>
+
 <form action='signup_form.php' method='post'>
-*****
+<input type='text' placeholder='First Name' name='first_name' class='form_input'required/><br>
+<input type='text' placeholder='Last Name' name='last_name' class='form_input' required/><br>
+<input type='email' placeholder='Email' name='email' class='form_input' required/><br>
+<input type='password' placeholder='Password' name='password' class='form_input' required/><br>
+<input type='password' placeholder='Confirm Password' name='confirm_password' class='form_input' required/><br>
+<input type="checkbox" name="is_instructor" value='Instructor'style='margin:6px 0px 6px 0px;'> Is Instructor?<br>
+<input type='submit' value='Sign Up' style='background:#59af50;color:#fff;width:30%;height:8%;margin:6px 0px 6px 0px;'/>
 </form>
 
-<a href='login.php' style='text-decoration:none;float:right;'><h4 style='color:#000;margin:0px;'>Already have an account?LogIn</h4></a>
+
+<a href='login.php' style='text-decoration:none;float:right;'><h4 style='color:#000;margin:0px;padding-right:20px;'>Already have an account?LogIn</h4></a>
+
 </div>
+
+
+
 </div></center>
+
+
+
 </div>
+
+
+
+
+
+
+
+
 </body>
 </html>
