a6aba9aa5e4b0673a232fdcfb87906dca5abac1c
Vinay Linginedi <43011798+vinaylinginedi@users.noreply.github.com>
2019-09-18 02:34:48 -0500

    Styling Css for consistence

--- instructor_dashboard_codeword.php
+++ instructor_dashboard_codeword.php
@@ -15,12 +15,12 @@ padding:0px;
 .header{
 width:100%;
 height:10%;
-background:#59af50;
+background:#0c0032;
 }
 .name{
 margin:0px;
 padding:22px 22px 0px 10px;
-color:#fff;
+color:#240090;
 float:left;
 }
 .body{
@@ -31,17 +31,17 @@ height:90%;
 
 width:90%;
 height:85%;
-background:#aed581;
+background:#190061;
 margin:0px auto;
 }
 .tab_bar{
 width:100%;
 height:10%;
-background:#59af50;
+padding-bottom:5px;
 }
 
 .tab_name{
-width:33.33%;
+width:25%;
 height:100%;
 color:#fff;
 float:left;
@@ -51,11 +51,11 @@ padding-top:10px;
 }
 
 .tab_name:hover{
-background:#85b887;
+background:#3500D3;
 transition:2s ease;
 }
 .active{
-background:#85b887;
+background:#3500D3;
 }
 
 
@@ -63,18 +63,20 @@ background:#85b887;
 </head>
 <body>
 <div class='header'>
-<h2 class ='name'>CODEWORD</h2>
-<a href='logout.php' style='text-decoration:none;'><h2 class ='name' style='float:right;'>Logout</h2></a>
+<!--<h2 id='name'>CODEWORD</h2>-->
+<img src="img.jpg" alt="img" style="float:center;width:200px;height:64px;">
+<a href='logout.php' style='text-decoration:none;'><h2 class ='name' style='float:right;color:#fff;'>Logout</h2></a>
 
 </div>
 <div class='body'>
-<h1 style='color:#9d9d9d;padding-left:22px; '>Instructor Dashboard</h1>
+<h1 style='color:#00000;padding-left:530px; '>Instructor Dashboard</h1>
 
 <div class='body_main'>
 <div class='tab_bar'>
 <a href='instructor_dashboard.php' style='text-decoration:none;'><h3 class='tab_name'>Add Course</h3></a>
 <a href='instructor_dashboard_course.php' style='text-decoration:none;'><h3 class='tab_name'>Course</h3></a>
 <a href='#' style='text-decoration:none;'><h3 class='tab_name active'>Codeword</h3></a>
+<a href='student_dashboard.php' style='text-decoration:none;'><h3 class='tab_name'>Student Dashboard</h3></a>
 </div>
 <div class='card_view_bar'></div>
 
