ce157d8c571b8c74fd43af78b63215a38eb24baf
Gopi Krishna Amara <43010422+gopi1506@users.noreply.github.com>
2019-09-17 19:46:59 -0500

    Craete instructor_course_detail_view.php
    Add code for template view for courses created

---
+++ instructor_course_detail_view.php
@@ -0,0 +1,70 @@
+<?php
+session_start();
+?>
+<?php
+error_reporting(0);
+?>
+<html>
+<head>
+<title>Instructor Dashboard</title>
+<style>
+body{
+margin:0px;
+padding:0px;
+}
+.header{
+width:100%;
+height:10%;
+background:#59af50;
+margin-bottom:12px;
+}
+.name{
+margin:0px;
+padding:22px 22px 0px 10px;
+color:#fff;
+float:left;
+}
+.body{
+width:90%;
+height:85%;
+margin:0px auto;
+background:#dcedc8;
+}
+.body_header{
+width:100%;
+height:15%;
+background:#59af50;
+}
+.top{
+width:100%;
+height:60%;
+}
+.bottom{
+width:100%;
+height:40%;
+}
+input{
+height:40px;
+border:1px;
+color:#fff;
+}
+.body_main{
+width:100%;
+height:85%;
+padding-left:12px;
+}
+
+
+
+</style>
+</head>
+<body>
+<div class='header'>
+<h2 class ='name'>CODEWORD</h2>
+<a href='logout.php' style='text-decoration:none;'><h2 class ='name' style='float:right;'>Logout</h2></a>
+</div>
+<div class='body'>
+<div class='body_header'>
+<?php?>
+</body>
+</html>
