264804ef4310fbeb505346c658ec2d3df58c2990
Vinay Linginedi <43011798+vinaylinginedi@users.noreply.github.com>
2019-09-18 02:01:31 -0500

    CSS styling modified in index

--- index.php
+++ index.php
@@ -15,19 +15,20 @@ padding:0px;
 .header{
 width:100%;
 height:10%;
-background:#59af50;
+background:#0c0032;
 }
 .body{
 width:100%;
 height:90%;
-background:#aed581;
+background:#190061;
 }
 
 #name{
 margin:0px;
 padding:22px 0px 0px 10px;
-color:#fff;
+color:#240090;
 }
+
 .signup{
 width:50%;
 height:60%;
@@ -36,12 +37,12 @@ padding-top:30px;
 .mini-header{
 width:100%;
 height:10%;
-background:#59af50;
+background:#282828;
 }
 .mini-body{
 width:100%;
-height:90%;
-background:#dcedc9;
+height:95%;
+background:#3500D3;
 }
 .form_input{
 width:80%;
@@ -49,17 +50,15 @@ height:8%;
 border:0px;
 margin:8px 0px 8px 0px;
 padding-left:4px;
-
 }
-
-
 
 </style>
 </head>
 <body>
 
 <div class='header'>
-<h2 id='name'>CODEWORD</h2>
+<!--<h2 id='name'>CODEWORD</h2>-->
+<img src="img.jpg" alt="img" style="float:center;width:200px;height:65px;">
 </div>
 
 
@@ -68,7 +67,7 @@ padding-left:4px;
 <center><div class='signup'>
 
 <div class='mini-header'>
-<h2 style='color:#fff;padding-top:10px;'>SIGN UP</h2>
+<h2 style='color:#fff;padding-top:5px;'>SIGN UP</h2>
 </div>
 <div class='mini-body'>
 
@@ -78,29 +77,13 @@ padding-left:4px;
 <input type='email' placeholder='Email' name='email' class='form_input' required/><br>
 <input type='password' placeholder='Password' name='password' class='form_input' required/><br>
 <input type='password' placeholder='Confirm Password' name='confirm_password' class='form_input' required/><br>
-<input type="checkbox" name="is_instructor" value='Instructor'style='margin:6px 0px 6px 0px;'> Is Instructor?<br>
-<input type='submit' value='Sign Up' style='background:#59af50;color:#fff;width:30%;height:8%;margin:6px 0px 6px 0px;'/>
+<input type="checkbox" name="is_instructor" value='Instructor'><h5 style='margin:6px 0px 6px 0px;'> Required Instructor access ?</h5><br>
+<input type='submit' value='Sign Up' style='background:#282828;color:#fff;width:30%;height:8%;margin:6px 0px 6px 0px;'/>
 </form>
 
-
-<a href='login.php' style='text-decoration:none;float:right;'><h4 style='color:#000;margin:0px;padding-right:20px;'>Already have an account?LogIn</h4></a>
-
+<a href='login.php' style='text-decoration:none;float:center;'><h4 style='color:#fff;margin:0px;'>Existing User? LogIn here</h4></a>
 </div>
-
-
-
 </div></center>
-
-
-
 </div>
-
-
-
-
-
-
-
-
 </body>
 </html>
