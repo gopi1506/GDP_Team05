11d029452d5c4fa10d6c7f76565375c12e9822f3
Gopi Krishna Amara <43010422+gopi1506@users.noreply.github.com>
2019-09-17 20:28:07 -0500

    Update login.php
    Add styling for login form

--- login.php
+++ login.php
@@ -1,76 +1,100 @@
 <?php
 session_start();
 ?>
-
+<?php
+error_reporting(0);
+?>
 <html>
-    <head>
-        <title>LogIn</title>
-        <style>
-            body{
-                margin:0px;
-                padding:0px;
-            }
-            .header{
-                width:100%;
-                height:10%;
-                background:#b3b3b3;
-            }
-            .body{
-                width:100%;
-                height:90%;
-                background:#4d4d4d;
-            }
-            #name{
-                margin:0px;
-                padding:22px 0px 0px 10px;
-                color:#000000;
-            }
-            .signup{
-                width:50%;
-                height:40%;
-                padding-top:30px;
-            }
-            .mini-header{
-                width:100%;
-                height:20%;
-                background:#cccccc;
-            }
-            .mini-body{
-                width:100%;
-                height:80%;
-                background:#808080;
-            }
-            .form_input{
-                width:80%;
-                height:15%;
-                border:0px;
-                margin:8px 0px 8px 0px;
-                padding-left:4px;
+<head>
+<title>LogIn</title>
+<style>
+body{
+margin:0px;
+padding:0px;
+}
+.header{
+width:100%;
+height:10%;
+background:#59af50;
+}
+.body{
+width:100%;
+height:90%;
+background:#aed581;
+}
 
-            }
-        </style>
-    </head>
-    <body>
-        <div class='header'>
-            <h2 id='name'>CODEWORD</h2>
-        </div>
-        <div class='body'>
-            <center>
-                <div class='signup'>
-                    <div class='mini-header'>
-                        <h2 style='color:#000000;padding-top:10px;'>LOGIN</h2>
-                    </div>
-                    <div class='mini-body'>
-                        <form action='login_form.php' method='post'>
-                            <input type='email' placeholder='Email' name='email' class='form_input' required/><br>
-                            <input type='password' placeholder='Password' name='password' class='form_input' required/><br>
-                            <input type='submit' value='LogIn' style='background:#000000;color:#fff;width:30%;height:15%;margin:6px 0px 6px 0px;'/>
-                        </form>
-                        <a href='#' style='text-decoration:none;float:left;'><h4 style='color:#000;margin:0px;padding-left:20px;'>Forgot Password?</h4></a>
-                        <a href='index.php' style='text-decoration:none;float:right;'><h4 style='color:#000;margin:0px;padding-right:20px;'>Don't have an account?SignUp</h4></a>
-                    </div>
-                </div>
-            </center>
-        </div>
-    </body>
+#name{
+margin:0px;
+padding:22px 0px 0px 10px;
+color:#fff;
+}
+.signup{
+width:50%;
+height:40%;
+padding-top:30px;
+}
+.mini-header{
+width:100%;
+height:20%;
+background:#59af50;
+}
+.mini-body{
+width:100%;
+height:80%;
+background:#dcedc9;
+}
+.form_input{
+width:80%;
+height:15%;
+border:0px;
+margin:8px 0px 8px 0px;
+padding-left:4px;
+
+}
+
+</style>
+</head>
+<body>
+
+<div class='header'>
+<h2 id='name'>CODEWORD</h2>
+</div>
+
+
+<div class='body'>
+
+<center><div class='signup'>
+
+<div class='mini-header'>
+<h2 style='color:#fff;padding-top:10px;'>LOGIN</h2>
+</div>
+<div class='mini-body'>
+
+<form action='login_form.php' method='post'>
+<input type='email' placeholder='Email' name='email' class='form_input' required/><br>
+<input type='password' placeholder='Password' name='password' class='form_input' required/><br>
+<input type='submit' value='LogIn' style='background:#59af50;color:#fff;width:30%;height:15%;margin:6px 0px 6px 0px;'/>
+</form>
+<a href='#' style='text-decoration:none;float:left;'><h4 style='color:#000;margin:0px;padding-left:20px;'>Forgot Password?</h4></a>
+
+<a href='index.php' style='text-decoration:none;float:right;'><h4 style='color:#000;margin:0px;padding-right:20px;'>Don't have an account?SignUp</h4></a>
+
+</div>
+
+
+
+</div></center>
+
+
+
+</div>
+
+
+
+
+
+
+
+
+</body>
 </html>
