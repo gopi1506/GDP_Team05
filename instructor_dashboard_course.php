b368942bc3d7722bae9da197b82fa2590b0e290d
Gopi Krishna Amara <43010422+gopi1506@users.noreply.github.com>
2019-09-17 19:59:15 -0500

    Update instructor_dashboard_course.php
    Add php code for database model and controlling part for displaying created courses

--- instructor_dashboard_course.php
+++ instructor_dashboard_course.php
@@ -74,6 +74,63 @@ color:#000000;
 <a href='instructor_dashboard_codeword.php' style='text-decoration:none;'><h3 class='tab_name'>Codeword</h3></a>
 </div>
 <div class='card_view_bar'></div>
+  <?php
+$email = $_SESSION["email"];
+
+$db = mysqli_connect('localhost', 'root', '', 'gdp') or die('error connecting to mysql db');
+$query = "select * from course where instructor_email='$email'";
+$query2 = mysqli_query($db, $query) or die('error querying db');
+
+				while($row = mysqli_fetch_array($query2))
+					{
+					$course_name = $row['course_name'];
+					$course_code = $row['course_code'];
+					$start_date = $row['start_date'];
+					$end_date = $row['end_date'];
+					$start_survey = $row['start_survey'];
+					$end_survey = $row['end_survey'];
+					$codeword_assigned = $row['published'];
+					$published = "";
+					$background = "";
+					
+					
+					echo  "<div class='delete_class'>";
+						echo "<div class='card_main'>";
+						echo "<form action='instructor_course_detail_view.php' method='post'>";
+					echo "<input type='hidden' name='course_code' value='".$course_code."'/>";
+					echo "<button style='padding:0px;width:100%;margin-top:12px;border-radius: 50%;'>";
+  					echo "<div class='post_header'>";
+  					echo "<p class='txt_user'>".$course_name."</p>";
+  					echo "<p class='txt_user_description'>Start Date: ".$start_date."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;End Date: ".$end_date."</p>";
+    				echo "</div>";
+					echo "<div class='card_content'>";
+  					echo "<p class='txt_title'>Acknowledged: </p>";
+  					echo "<p class='txt_title'>Start Link: ".$start_survey."</p>";
+  					echo "<p class='txt_title'>End Link: ".$end_survey."</p>";
+  					if($codeword_assigned == 'false'){
+  						$published = "Not assigned";
+  						$background = "red";
+  					}else{
+  						$published = "Assigned";
+  						$background = "green";
+  					}//end of if else statement
+  					
+  					echo "<p class='txt_title' style='background:".$background.";text-align:center;padding:3px 0px;'>".$published."</p>";
+  					echo "</div>";
+  					
+  					
+ 					echo "<div class='box'></div>";
+
+ 					echo "</div>";
+ 					echo "</button>";
+ 					echo "</form>";
+					echo "</div>";
+					
+								
+					}//end of while loop
+
+
+?>
 </div>
 </div>
 </body>
