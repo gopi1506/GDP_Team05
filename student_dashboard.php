84f7cacac6a55eb7ae84d2c849f188008a15a263
Vinay Linginedi <43011798+vinaylinginedi@users.noreply.github.com>
2019-09-26 23:32:07 -0500

    Add layout and logout feature

--- student_dashboard.php
+++ student_dashboard.php
@@ -1,9 +1,21 @@
 <?php
 session_start();
 ?>
-<?php
-//student Dashboard
+<html>
+<head>
+<title>Student Dashboard</title>
+<style>
+</style>
+  
+</head>
+<body>
+<div class='header'>
+<h2 class ='name'>CODEWORD</h2>
+<a href='logout.php' style='text-decoration:none;'><h2 class ='name' style='float:right;'>Logout</h2></a>
+</div>
+</body>
+</html>
+  
 
-echo "Student Dashboard";
 
-?>
+
