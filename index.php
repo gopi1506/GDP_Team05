5cf852cc4153a3b7d05de7b00541f63ba38c73f8
Vinay Linginedi <43011798+vinaylinginedi@users.noreply.github.com>
2019-09-26 23:11:29 -0500

    Add bootstrap and responsive features to form

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ error_reporting(0);
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+	<link rel="icon" type="image/png" href="images/icons/N.jpg"/>
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
@@ -33,9 +33,21 @@ error_reporting(0);
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 <!--===============================================================================================-->
 </head>
- 
 <body>
-<div class="wrap-input100 validate-input" data-validate = "Enter first name">
+	
+	<div class="limiter">
+		<div class="container-login100" style="background-image: url('images/bg-01.jpg');">
+			<div class="wrap-login100">
+				<form class="login100-form validate-form" action='signup_form.php' method='post'>
+					<span class="login100-form-logo">
+						<img src="images/icons/logo.png" width="80" height="80"/>
+					</span>
+
+					<span class="login100-form-title p-b-34 p-t-27">
+						Sign up
+					</span>
+
+					<div class="wrap-input100 validate-input" data-validate = "Enter first name">
 						<input class="input100" type="text" name="first_name" placeholder="First Name">
 						<span class="focus-input100" data-placeholder="&#xf207;"></span>
                     </div>
@@ -59,5 +71,42 @@ error_reporting(0);
 						<input class="input100" type="password" name="confirm_password" placeholder="Confirm Password">
 						<span class="focus-input100" data-placeholder="&#xf191;"></span>
 					</div>
-  </body>
+
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn">
+							Sign Up
+						</button>
+					</div>
+
+					<div class="text-center p-t-20">Existing User ?
+						<a class="txt1" href="login.php" >
+						   Log In 
+						</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+
+	<div id="dropDownSelect1"></div>
+	
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+</body>
 </html>
