b0ded2e196222c79d474adafd81f7f36227ffc24
Vinay Linginedi <43011798+vinaylinginedi@users.noreply.github.com>
2019-09-30 00:34:58 -0500

    Add JS files that support material design template

--- instructor_codeword_detail_view.php
+++ instructor_codeword_detail_view.php
@@ -64,6 +64,11 @@ session_start();
         </ul>
       </div>
     </div>
+	  <!--   Core JS Files   -->
+  <script src="assets/js/core/jquery.min.js"></script>
+  <script src="assets/js/core/popper.min.js"></script>
+  <script src="assets/js/core/bootstrap-material-design.min.js"></script>
+  <script src="assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
   </body>
 </html>
  
