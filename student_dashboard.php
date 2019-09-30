00b025630cc4d4be90b09d76eb293b00e0d79807
Harika Naidu <nagaharika.n@gmail.com>
2019-09-30 17:52:15 -0500

    Update student dashboard with body,html tags

--- student_dashboard.php
+++ student_dashboard.php
@@ -326,4 +326,12 @@ error_reporting(0);
           });
         });
       });
-  </script>
+    </script>
+    <script>
+      $(document).ready(function() {
+        // Javascript method's body can be found in assets/js/demos.js
+        md.initDashboardPageCharts();
+      });
+  </script>        
+  </body>
+</html>
