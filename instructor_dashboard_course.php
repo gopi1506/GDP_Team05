7ac2eecccf93fa79e368bce1dba3a77671a055f7
gopi <gopi3ka@gmail.com>
2019-10-02 20:32:03 -0500

    Modify instructor dashboard course page

--- instructor_dashboard_course.php
+++ instructor_dashboard_course.php
@@ -2,68 +2,399 @@
 session_start();
 ?>
 <?php
+$email = $_SESSION["email"];
 error_reporting(0);
 ?>
-<html>
+
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-<title>Instructor Dashboard</title>
-<style>
-body{
-margin:0px;
-padding:0px;
-}
-.header{
-width:100%;
-height:10%;
-background:#265ca7;
-}
-.name{
-margin:0px;
-padding:22px 22px 0px 10px;
-color:#fff;
-float:left;
-}
-.body{
-width:100%;
-height:90%;
-}
-.body_main{
+  <meta charset="utf-8" />
+  <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="assets/img/favicon.png">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>
+    Add Course
+  </title>
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+  <!--     Fonts and icons     -->
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+  <!-- CSS Files -->
+  <link href="assets/css/material-dashboard.css?v=2.1.1" rel="stylesheet" />
+  <!-- CSS Just for demo purpose, don't include it in your project -->
+  <link href="assets/demo/demo.css" rel="stylesheet" />
 
-width:90%;
-height:85%;
-background:#bbd7ff;
-margin:0px auto;
-}
-.tab_bar{
-width:100%;
-height:10%;
-background:#265ca7;
-}
+  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+</head>
 
-.tab_name{
-width:33.33%;
-height:100%;
-color:#fff;
-float:left;
-text-align:center;
-margin:0px;
-padding-top:10px;
-}
+<body class="">
+  <div class="wrapper ">
+  <div class="sidebar" data-color="purple" data-background-color="white" data-image="assets/img/sidebar-1.jpg">
 
-.tab_name:hover{
-background:#85b887;
-transition:2s ease;
-}
-.active{
-background:#94b1ee;
-}
+      <div class="logo">
+        <a href="#" class="simple-text logo-normal">
+          <?php
+          $db = mysqli_connect('localhost', 'root', '', 'gdp') or die('error connecting to mysql db');
+ $query = "select * from user_details where email='$email'";
+     $query2 = mysqli_query($db, $query) or die('error querying db');
+while($row = mysqli_fetch_array($query2))
+{
+$first_name = $row['first_name'];
+$last_name = $row['last_name'];
+          echo $first_name." ".$last_name;
+          }//end of while loop  
+          ?>
+        </a>
+      </div>
+      <div class="sidebar-wrapper">
+        <ul class="nav">
+          <li class="nav-item ">
+            <a class="nav-link" href="instructor_dashboard.php">
+              <i class="material-icons">dashboard</i>
+              <p>Course</p>
+            </a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="instructor_dashboard_course.php">
+              <i class="material-icons">content_paste</i>
+              <p>Add Course</p>
+            </a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="instructor_dashboard_codeword.php">
+              <i class="material-icons">library_books</i>
+              <p>Codeword</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="main-panel">
+      <!-- Navbar -->
+      <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top ">
+        <div class="container-fluid">
+          <div class="navbar-wrapper">
+            <a class="navbar-brand" href="#">Add Course</a>
+          </div>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="navbar-toggler-icon icon-bar"></span>
+            <span class="navbar-toggler-icon icon-bar"></span>
+            <span class="navbar-toggler-icon icon-bar"></span>
+          </button>
+           <div class="collapse navbar-collapse justify-content-end">
+            <form class="navbar-form"></form>
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="logout.php">
+                  <i class="material-icons">logout</i>
+                  <p class="d-lg-none d-md-block">
+                    Logout
+                  </p>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+          <!-- End Navbar -->
+      <?php
+      $current_date = date("m/d/Y");
+      $final_date = date('m/d/Y', strtotime('+4 month', time()));
+      ?>
+ 
+      <div class="content">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="card">
+                <div class="card-header card-header-primary">
+                  <h4 class="card-title">Add Course</h4>
+                  <p class="card-category">Complete the form to add course</p>
+                </div>
+                <div class="card-body">
+                  <form action='add_course.php' method='post' enctype="multipart/form-data">
+                    <div class="row">
+                      <div class="col-md-12">
+                        <div class="form-group">
+                          <input type="text" name="course" class="form-control" placeholder="Course Name" required>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="col-md-12">
+                        <div class="form-group">
+                          <label class="bmd-label-floating">Select Date</label>
+                          <input type='text'  class="form-control" name='date' value="<?php echo $current_date; ?> - <?php echo $final_date; ?>" required/>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="col-md-4">
+                     
+                      <div class="input-group">
+                        
+                     <input type="file" name='file' class="form-control inputFileVisible" placeholder="Single File" required>
+                      <span class="input-group-btn btn btn-fab btn-round btn-primary">
+                      <i class="material-icons">attach_file</i>
+                     </span>
+                     </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="form-group">
+                          <input type='text'  class="form-control" name='start' placeholder='Start Survey' />
+                     </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                          <input type='text'  class="form-control" name='end' placeholder='End Survey' />
+                     </div>
+                    </div>
 
-</style>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
+
+                        </div>
+                      <div class="row">
+                      <div class="col-md-8">
+                        <div class="form-group">
+                       
+    <select class="form-control" id="exampleFormControlSelect1" name="codeword">
+      <option value="Select Codeword">Select Codeword</option>
+      
+    </select>
+
+        </div>
+        </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary pull-right">Submit</button>
+                    <div class="clearfix"></div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  <!--   Core JS Files   -->
+  <script src="assets/js/core/jquery.min.js"></script>
+  <script src="assets/js/core/popper.min.js"></script>
+  <script src="assets/js/core/bootstrap-material-design.min.js"></script>
+  <script src="assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+  <!-- Plugin for the momentJs  -->
+  <script src="assets/js/plugins/moment.min.js"></script>
+  <!--  Plugin for Sweet Alert -->
+  <script src="assets/js/plugins/sweetalert2.js"></script>
+  <!-- Forms Validations Plugin -->
+  <script src="assets/js/plugins/jquery.validate.min.js"></script>
+  <!-- Plugin for the Wizard, full documentation here: https://github.com/VinceG/twitter-bootstrap-wizard -->
+  <script src="assets/js/plugins/jquery.bootstrap-wizard.js"></script>
+  <!--	Plugin for Select, full documentation here: http://silviomoreto.github.io/bootstrap-select -->
+  <script src="assets/js/plugins/bootstrap-selectpicker.js"></script>
+  <!--  Plugin for the DateTimePicker, full documentation here: https://eonasdan.github.io/bootstrap-datetimepicker/ -->
+  <script src="assets/js/plugins/bootstrap-datetimepicker.min.js"></script>
+  <!--  DataTables.net Plugin, full documentation here: https://datatables.net/  -->
+  <script src="assets/js/plugins/jquery.dataTables.min.js"></script>
+  <!--	Plugin for Tags, full documentation here: https://github.com/bootstrap-tagsinput/bootstrap-tagsinputs  -->
+  <script src="assets/js/plugins/bootstrap-tagsinput.js"></script>
+  <!-- Plugin for Fileupload, full documentation here: http://www.jasny.net/bootstrap/javascript/#fileinput -->
+  <script src="assets/js/plugins/jasny-bootstrap.min.js"></script>
+  <!--  Full Calendar Plugin, full documentation here: https://github.com/fullcalendar/fullcalendar    -->
+  <script src="assets/js/plugins/fullcalendar.min.js"></script>
+  <!-- Vector Map plugin, full documentation here: http://jvectormap.com/documentation/ -->
+  <script src="assets/js/plugins/jquery-jvectormap.js"></script>
+  <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+  <script src="assets/js/plugins/nouislider.min.js"></script>
+  <!-- Include a polyfill for ES6 Promises (optional) for IE11, UC Browser and Android browser support SweetAlert -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/core.js"></script>
+  <!-- Library for adding dinamically elements -->
+  <script src="assets/js/plugins/arrive.min.js"></script>
+  <!--  Google Maps Plugin    -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+  <!-- Chartist JS -->
+  <script src="assets/js/plugins/chartist.min.js"></script>
+  <!--  Notifications Plugin    -->
+  <script src="assets/js/plugins/bootstrap-notify.js"></script>
+  <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
+  <script src="assets/js/material-dashboard.js?v=2.1.1" type="text/javascript"></script>
+  <!-- Material Dashboard DEMO methods, don't include it in your project! -->
+  <script src="assets/demo/demo.js"></script>
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
-<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+<script>
+    $(document).ready(function() {
+      $().ready(function() {
+        $sidebar = $('.sidebar');
 
+        $sidebar_img_container = $sidebar.find('.sidebar-background');
+
+        $full_page = $('.full-page');
+
+        $sidebar_responsive = $('body > .navbar-collapse');
+
+        window_width = $(window).width();
+
+        fixed_plugin_open = $('.sidebar .sidebar-wrapper .nav li.active a p').html();
+
+        if (window_width > 767 && fixed_plugin_open == 'Dashboard') {
+          if ($('.fixed-plugin .dropdown').hasClass('show-dropdown')) {
+            $('.fixed-plugin .dropdown').addClass('open');
+          }
+
+        }
+
+        $('.fixed-plugin a').click(function(event) {
+          // Alex if we click on switch, stop propagation of the event, so the dropdown will not be hide, otherwise we set the  section active
+          if ($(this).hasClass('switch-trigger')) {
+            if (event.stopPropagation) {
+              event.stopPropagation();
+            } else if (window.event) {
+              window.event.cancelBubble = true;
+            }
+          }
+        });
+
+        $('.fixed-plugin .active-color span').click(function() {
+          $full_page_background = $('.full-page-background');
+
+          $(this).siblings().removeClass('active');
+          $(this).addClass('active');
+
+          var new_color = $(this).data('color');
+
+          if ($sidebar.length != 0) {
+            $sidebar.attr('data-color', new_color);
+          }
+
+          if ($full_page.length != 0) {
+            $full_page.attr('filter-color', new_color);
+          }
+
+          if ($sidebar_responsive.length != 0) {
+            $sidebar_responsive.attr('data-color', new_color);
+          }
+        });
+
+        $('.fixed-plugin .background-color .badge').click(function() {
+          $(this).siblings().removeClass('active');
+          $(this).addClass('active');
+
+          var new_color = $(this).data('background-color');
+
+          if ($sidebar.length != 0) {
+            $sidebar.attr('data-background-color', new_color);
+          }
+        });
+
+        $('.fixed-plugin .img-holder').click(function() {
+          $full_page_background = $('.full-page-background');
+
+          $(this).parent('li').siblings().removeClass('active');
+          $(this).parent('li').addClass('active');
+
+
+          var new_image = $(this).find("img").attr('src');
+
+          if ($sidebar_img_container.length != 0 && $('.switch-sidebar-image input:checked').length != 0) {
+            $sidebar_img_container.fadeOut('fast', function() {
+              $sidebar_img_container.css('background-image', 'url("' + new_image + '")');
+              $sidebar_img_container.fadeIn('fast');
+            });
+          }
+
+          if ($full_page_background.length != 0 && $('.switch-sidebar-image input:checked').length != 0) {
+            var new_image_full_page = $('.fixed-plugin li.active .img-holder').find('img').data('src');
+
+            $full_page_background.fadeOut('fast', function() {
+              $full_page_background.css('background-image', 'url("' + new_image_full_page + '")');
+              $full_page_background.fadeIn('fast');
+            });
+          }
+
+          if ($('.switch-sidebar-image input:checked').length == 0) {
+            var new_image = $('.fixed-plugin li.active .img-holder').find("img").attr('src');
+            var new_image_full_page = $('.fixed-plugin li.active .img-holder').find('img').data('src');
+
+            $sidebar_img_container.css('background-image', 'url("' + new_image + '")');
+            $full_page_background.css('background-image', 'url("' + new_image_full_page + '")');
+          }
+
+          if ($sidebar_responsive.length != 0) {
+            $sidebar_responsive.css('background-image', 'url("' + new_image + '")');
+          }
+        });
+
+        $('.switch-sidebar-image input').change(function() {
+          $full_page_background = $('.full-page-background');
+
+          $input = $(this);
+
+          if ($input.is(':checked')) {
+            if ($sidebar_img_container.length != 0) {
+              $sidebar_img_container.fadeIn('fast');
+              $sidebar.attr('data-image', '#');
+            }
+
+            if ($full_page_background.length != 0) {
+              $full_page_background.fadeIn('fast');
+              $full_page.attr('data-image', '#');
+            }
+
+            background_image = true;
+          } else {
+            if ($sidebar_img_container.length != 0) {
+              $sidebar.removeAttr('data-image');
+              $sidebar_img_container.fadeOut('fast');
+            }
+
+            if ($full_page_background.length != 0) {
+              $full_page.removeAttr('data-image', '#');
+              $full_page_background.fadeOut('fast');
+            }
+
+            background_image = false;
+          }
+        });
+
+        $('.switch-sidebar-mini input').change(function() {
+          $body = $('body');
+
+          $input = $(this);
+
+          if (md.misc.sidebar_mini_active == true) {
+            $('body').removeClass('sidebar-mini');
+            md.misc.sidebar_mini_active = false;
+
+            $('.sidebar .sidebar-wrapper, .main-panel').perfectScrollbar();
+
+          } else {
+
+            $('.sidebar .sidebar-wrapper, .main-panel').perfectScrollbar('destroy');
+
+            setTimeout(function() {
+              $('body').addClass('sidebar-mini');
+
+              md.misc.sidebar_mini_active = true;
+            }, 300);
+          }
+
+          // we simulate the window Resize so the charts will get updated in realtime.
+          var simulateWindowResize = setInterval(function() {
+            window.dispatchEvent(new Event('resize'));
+          }, 180);
+
+          // we stop the simulation of Window Resize after the animations are completed
+          setTimeout(function() {
+            clearInterval(simulateWindowResize);
+          }, 1000);
+
+        });
+      });
+    });
+  </script>
 <script>
 $(function() {
   $('input[name="date"]').daterangepicker({
@@ -74,49 +405,9 @@ $(function() {
 });
 </script>
 
-</head>
-<body>
-<div class='header'>
-<h2 class ='name'>CODEWORD</h2>
-<a href='logout.php' style='text-decoration:none;'><h2 class ='name' style='float:right;'>Logout</h2></a>
-
-</div>
-<div class='body'>
-<h1 style='color:#9d9d9d;padding-left:22px; '>Instructor Dashboard</h1>
-
-<div class='body_main'>
-<div class='tab_bar'>
-<a href='#' style='text-decoration:none;'><h3 class='tab_name active' >Add Course</h3></a>
-<a href='instructor_dashboard.php' style='text-decoration:none;'><h3 class='tab_name'>Course</h3></a>
-<a href='instructor_dashboard_codeword.php' style='text-decoration:none;'><h3 class='tab_name'>Codeword</h3></a>
-</div>
-<div class='card_view_bar'></div>
-<center>
-<h2 style='color:#fff;'>Add Course</h2>
-<?php
-$current_date = date("m/d/Y");
-$final_date = date('m/d/Y', strtotime('+4 month', time()));
-?>
-
-
-<form action='add_course.php' method='post' enctype="multipart/form-data">
-<input type='text' style='width:60%;height:40px;margin-bottom:6px;' placeholder='Course Name' name='course' required/></br>
-<input type='text' style='width:60%;height:40px;margin-bottom:6px;' name='date' value="<?php echo $current_date; ?> - <?php echo $final_date; ?>" required/></br>
-<input type='file' style='width:30%;height:40px;margin-bottom:6px;' name='file' value='Upload' required/> Upload Students CSV</br>
-<input type='text' style='width:60%;height:40px;margin-bottom:6px;' placeholder='Start survey link' name="start"/></br>
-<input type='text' style='width:60%;height:40px;margin-bottom:6px;' placeholder='End survey link' name="end"/></br>
-<select style='width:60%;height:40px;margin-bottom:6px;' name='codeword'>
-  <option value="Select Codeword" >Select Codeword</option>
-  <option value="a." >a.</option>
-</select></br>
-<input type='submit' style='width:30%;height:40px;margin-bottom:6px;font-size:20px;' value='Add Course'/>
 
 
 
-
-</form>
-</center>
-</div>
-</div>
 </body>
+
 </html>
