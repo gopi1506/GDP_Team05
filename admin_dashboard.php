8732afde3b4f3f41a23515fc063f313d59b70dec
Vinay Linginedi <43011798+vinaylinginedi@users.noreply.github.com>
2019-10-07 19:30:11 -0500

    Add navigation logic import from bootstrap

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -64,6 +64,34 @@ $last_name = $row['last_name'];
       </div>
     </div>
     <div class="main-panel">
+      <!-- Navbar -->
+      <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top ">
+        <div class="container-fluid">
+          <div class="navbar-wrapper">
+            <a class="navbar-brand" href="#">Admin Dashboard</a>
+          </div>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="navbar-toggler-icon icon-bar"></span>
+            <span class="navbar-toggler-icon icon-bar"></span>
+            <span class="navbar-toggler-icon icon-bar"></span>
+          </button>
+           <div class="collapse navbar-collapse justify-content-end">
+            <form class="navbar-form"></form>
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="logout.php">
+                  <i class="material-icons">logout</i>
+                  <p class="d-lg-none d-md-block">
+                    Logout
+                  </p>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <!-- End Navbar -->
       <!--   Core JS Files   -->
   <script src="assets/js/core/jquery.min.js"></script>
   <script src="assets/js/core/popper.min.js"></script>
