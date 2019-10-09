6c4b59946ed002989edb50f06669e35deb16dc71
Harika Naidu <nagaharika.n@gmail.com>
2019-10-09 09:26:15 -0500

    Add database query in instructor request file

--- request_instructor.php
+++ request_instructor.php
@@ -3,3 +3,12 @@ session_start();
 ?>
 <?php
 error_reporting(0);
+$email = $_SESSION["email"];
+
+$db = mysqli_connect('localhost', 'root', '', 'gdp') or die('error connecting to mysql db');
+
+$query = "insert into admin(email,ack) values ('$email','false')";
+$query2 = mysqli_query($db, $query);
+
+include 'student_dashboard.php';
+?>
