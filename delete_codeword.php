2dc19ccbd6e2d1ce55035bf74e5f594b3df4a953
Harika Naidu <nagaharika.n@gmail.com>
2019-10-08 22:08:35 -0500

    Add database query to delete codeword operation

--- delete_codeword.php
+++ delete_codeword.php
@@ -5,3 +5,19 @@ session_start();
 error_reporting(0);
 
 $codewordset_code = $_POST['codewordset_code'];
+
+$codewordset_code = $_POST['codewordset_code'];
+
+$db = mysqli_connect('localhost', 'root', '', 'gdp') or die('error connecting to mysql db');
+$query = "drop table ".$codewordset_code."";
+$query1 = "delete from codewordset where codewordset_code='$codewordset_code'";
+
+$query2 = mysqli_query($db, $query) or die('error querying db 123');
+$query33 = mysqli_query($db, $query1) or die('error querying db ');
+
+
+include('instructor_dashboard_codeword.php');
+echo " <script>alert('Sucesssfully Deleted Course');</script>  ";
+
+
+?>
