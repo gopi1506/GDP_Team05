3c64ea5fe51db9bbfc8786679d9620b1b5dd9513
Harika Naidu <nagaharika.n@gmail.com>
2019-10-23 09:15:01 -0500

    Edit database connection in the instructor request

--- request_instructor.php
+++ request_instructor.php
@@ -1,13 +1,11 @@
 <?php
-session_start();
+    session_start();
 ?>
 <?php
-error_reporting(0);
-$email = $_SESSION["email"];
-$db = mysqli_connect('localhost', 'root', '', 'gdp') or die('error connecting to mysql db');
-
-$query = "insert into admin(email,ack) values ('$email','false')";
-$query2 = mysqli_query($db, $query);
-
-include 'student_dashboard.php';
+    error_reporting(0);
+    $email = $_SESSION["email"];
+    $db = mysqli_connect('localhost', 'root', '', 'gdp') or die('error connecting to mysql db');
+    $query = "insert into admin(email,ack) values ('$email','false')";
+    $query2 = mysqli_query($db, $query);
+    include 'student_dashboard.php';
 ?>
