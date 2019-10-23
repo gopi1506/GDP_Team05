e2d84689104cbb4390916ac0289316380b7b9715
Harika Naidu <nagaharika.n@gmail.com>
2019-10-23 09:16:52 -0500

    Add indentation in login

--- logout.php
+++ logout.php
@@ -1,10 +1,8 @@
 <?php
-session_start();
+    session_start();
 ?>
 <?php
-
-session_unset();
-session_destroy();
-include 'login.php';
-
+    session_unset();
+    session_destroy();
+    include 'login.php';
 ?>
