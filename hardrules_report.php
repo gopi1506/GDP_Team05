0936cff88d42c90ebc8c32b39077e7ce47abd969
Harika Naidu <nagaharika.n@gmail.com>
2019-11-06 09:15:43 -0600

    Start the HTML implementation

--- hardrules_report.php
+++ hardrules_report.php
@@ -1,3 +1,8 @@
 <?php
 session_start();
 ?>
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
