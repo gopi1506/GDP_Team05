d0b66ebad122110fdfb993e7311275f9fb059873
gopi <gopi3ka@gmail.com>
2019-11-07 13:27:22 -0600

    Add control file for reporting soft rules

---
+++ softrules_report_code.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+?>
+<?php
+$codewordset_code = $_POST['codewordset'];
+$codeword = array();
+
+$db = mysqli_connect('localhost', 'root', '', 'gdp') or die('error connecting to mysql db');
+$query = "select * from ".$codewordset_code."";
+$query2 = mysqli_query($db, $query) or die('error querying db');
+
+while($row = mysqli_fetch_array($query2))
+{
+$codeword[] = $row['codeword'];
+
+}//end of while loop
+
+
+for($i=0;$i < count($codeword); $i++ ){
+
+    for($j=0;$j < count($codeword); $j++ ){
+
+        if($i != $j){
+            
+        $a = similar_text($codeword[$i],$codeword[$j],$percent);
+                echo $codeword[$i]."---------->".$codeword[$j]."--------------->".$percent;
+
+        }//end of if condition
+
+
+    }//end of inner for loop
+
+
+}//end of outer for loop
+
+
+
+
+
+
+
+
+
+
+?>
