431eb39319eeb762a243ba19499749cb758146b5
Vinay Linginedi <43011798+vinaylinginedi@users.noreply.github.com>
2019-11-07 12:37:13 -0600

    Add fetch code to retrieve data from database

--- softrules_report.php
+++ softrules_report.php
@@ -9,9 +9,25 @@ $db = mysqli_connect('localhost', 'root', '', 'gdp') or die('error connecting to
 $query = "select * from ".$codewordset_code."";
 $query2 = mysqli_query($db, $query) or die('error querying db');
 
+while($row = mysqli_fetch_array($query2))
+{
+$codeword[] = $row['codeword'];
 
+}//end of while loop
+
+
+for($i=0;$i < count($codeword); $i++ ){
+
+    for($j=0;$j < count($codeword); $j++ ){
+
+        if($i != $j){
+            
+        $a = similar_text($codeword[$i],$codeword[$j],$percent);
+                echo $codeword[$i]."---------->".$codeword[$j]."--------------->".$percent;
+
+        }//end of if condition
 
     }//end of inner for loop
 
-
 }//end of outer for loop
+?>
