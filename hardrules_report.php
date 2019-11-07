5fb3c7ac8b67db56a8c49e20d7c2afa6fa53135f
Harika Naidu <nagaharika.n@gmail.com>
2019-11-06 23:42:18 -0600

    Add the head part

--- hardrules_report.php
+++ hardrules_report.php
@@ -3,60 +3,58 @@ session_start();
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8" />
-  <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="assets/img/favicon.png">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-  <title>
-    Hard Rules Report
-  </title>
-  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
-  <!--     Fonts and icons     -->
-  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
-  <!-- CSS Files -->
-  <link href="assets/css/material-dashboard.css?v=2.1.1" rel="stylesheet" />
-  <!-- CSS Just for demo purpose, don't include it in your project -->
-  <link href="assets/demo/demo.css" rel="stylesheet" />
-</head>
-
-<body class="">
-  <div class="wrapper ">
-    <div class="sidebar" data-color="purple" data-background-color="white" data-image="assets/img/sidebar-1.jpg">
-
-      <div class="logo">
-        <a href="#" class="simple-text logo-normal">
-         <!-- something comes here -->
-        </a>
-      </div>
-      <div class="sidebar-wrapper">
-        <ul class="nav">
-          <li class="nav-item ">
-            <a class="nav-link" href="instructor_dashboard.php">
-              <i class="material-icons">dashboard</i>
-              <p>Dashboard</p>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8" />
+      <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+      <link rel="icon" type="image/png" href="assets/img/favicon.png">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+      <title>
+        Hard Rules Report
+      </title>
+      <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+      <!--     Fonts and icons     -->
+      <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+      <!-- CSS Files -->
+      <link href="assets/css/material-dashboard.css?v=2.1.1" rel="stylesheet" />
+      <!-- CSS Just for demo purpose, don't include it in your project -->
+      <link href="assets/demo/demo.css" rel="stylesheet" />
+    </head>
+    <body class="">
+      <div class="wrapper ">
+        <div class="sidebar" data-color="purple" data-background-color="white" data-image="assets/img/sidebar-1.jpg">
+          <div class="logo">
+            <a href="#" class="simple-text logo-normal">
+            <!-- something comes here -->
             </a>
-          </li>
-          <li class="nav-item ">
-            <a class="nav-link" href="instructor_dashboard_course.php">
-              <i class="material-icons">content_paste</i>
-              <p>Add Course</p>
-            </a>
-          </li>
-          <li class="nav-item ">
-            <a class="nav-link" href="instructor_dashboard_codeword.php">
-              <i class="material-icons">library_books</i>
-              <p>Codeword</p>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-    <div class="main-panel">
-      <!-- Navbar -->
-      <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top ">
+          </div>
+          <div class="sidebar-wrapper">
+            <ul class="nav">
+              <li class="nav-item ">
+                <a class="nav-link" href="instructor_dashboard.php">
+                  <i class="material-icons">dashboard</i>
+                    <p>Dashboard</p>
+                </a>
+              </li>
+              <li class="nav-item ">
+                <a class="nav-link" href="instructor_dashboard_course.php">
+                  <i class="material-icons">content_paste</i>
+                  <p>Add Course</p>
+                </a>
+              </li>
+              <li class="nav-item ">
+                <a class="nav-link" href="instructor_dashboard_codeword.php">
+                  <i class="material-icons">library_books</i>
+                  <p>Codeword</p>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="main-panel">
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top ">
         <div class="container-fluid">
           <div class="navbar-wrapper">
             <a class="navbar-brand" href="#">Hard Rules Report</a>
@@ -125,7 +123,7 @@ session_start();
 
 
 								
-								 <div class='card '>
+					<div class='card '>
             <div class='card-header card-header-primary'>
               <h4 class='card-title'>Hard Rules Report</h4>
             </div>
