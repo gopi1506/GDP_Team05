998cfdac3d0f53f64aa8d07de0937a7c558a7614
Harika Naidu <nagaharika.n@gmail.com>
2019-11-07 18:59:12 -0600

    Complete nav bar in hardrules report file

--- hardrules_report.php
+++ hardrules_report.php
@@ -55,10 +55,31 @@ session_start();
         <div class="main-panel">
           <!-- Navbar -->
           <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top ">
-          <div class="container-fluid">
-            <div class="navbar-wrapper">
-              <a class="navbar-brand" href="#">Hard Rules Report</a>
+            <div class="container-fluid">
+              <div class="navbar-wrapper">
+                <a class="navbar-brand" href="#">Hard Rules Report</a>
+              </div>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="navbar-toggler-icon icon-bar"></span>
+                <span class="navbar-toggler-icon icon-bar"></span>
+                <span class="navbar-toggler-icon icon-bar"></span>
+              </button>
+              <div class="collapse navbar-collapse justify-content-end">
+                <form class="navbar-form"></form>
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link" href="logout.php">
+                      <i class="material-icons">logout</i>
+                      <p class="d-lg-none d-md-block">
+                        Logout
+                      </p>
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
+          </nav><!-- End Navbar -->
   <script>
     $(document).ready(function() {
       $().ready(function() {
