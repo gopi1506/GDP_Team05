78e5b16edfb3cb8f2d92574c71810163eca70d0a
gopi <gopi3ka@gmail.com>
2019-11-15 14:39:38 -0600

    Add code for softrules report

---
+++ softrules_report.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+?>
+<?php
+error_reporting(0);
+$codewordset_code = $_POST['codewordset_code'];
+$codeword = $_POST['codeword'];
+
+$db = mysqli_connect('localhost', 'root', '', 'gdp') or die('error connecting to mysql db');
+$query1 = "delete from ".$codewordset_code." where codeword='$codeword'";
+
+$query33 = mysqli_query($db, $query1) or die('error querying db ');
+
+include('instructor_codeword_detail_view.php');
+//echo " <script>alert('Sucesssfully Deleted Course');</script>  ";
+
+echo $codewordset_code."    ".$codeword;
+
+?>
