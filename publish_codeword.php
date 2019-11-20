a188de5570e2b6aff5e0ee69e03a5b7a017ef642
gopi <gopi3ka@gmail.com>
2019-11-19 22:13:41 -0600

    Add code for publishing codeword set

---
+++ publish_codeword.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+?>
+<?php
+error_reporting(0);
+
+$codewordset_code = $_POST['codewordset_code'];
+
+$db = mysqli_connect('localhost', 'root', '', 'gdp') or die('error connecting to mysql db');
+$query = "UPDATE codewordset SET published ='true' where codewordset_code='$codewordset_code'";
+$query9 = mysqli_query($db, $query) or die('error querying db');
+
+include 'instructor_dashboard_codeword.php';
+
+?>
