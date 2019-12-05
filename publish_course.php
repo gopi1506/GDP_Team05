e060f558ab0b77f97667c4011dd1909cb1a39428
Vinay Linginedi <43011798+vinaylinginedi@users.noreply.github.com>
2019-12-04 20:46:02 -0600

    Update publish course with some conditions
    Changing the status of course as published after checking the if condition for codeword count and student count

--- publish_course.php
+++ publish_course.php
@@ -37,6 +37,59 @@ $codewordset_name = "";
 
                             $studentpercent = $studentcount + ($studentcount * (20/100));
 
+                            if($codewordscount > $studentpercent){
+                                $query7 = "UPDATE course SET published ='true' where course_code='$course_code'";
+                                $query9 = mysqli_query($db, $query7) or die('error querying db');
+                                
+                                //start of while loop for codeword
+
+                                $query20 = "select * from ".$codewordset_key."";
+                                $query21 = mysqli_query($db, $query20) or die('error querying db');
+                                while($row = mysqli_fetch_array($query21))
+                                 {
+                                    $codeword = $row['codeword'];
+                                    $temp[] = $codeword;
+                                   // print_r($temp);
+                                 }
+                                // end of while loop for codeword
+
+                                //start of while loop for students
+
+                                $query22 = "select * from ".$course_code."";
+                                $query23 = mysqli_query($db, $query22) or die('error querying db');
+                                while($row = mysqli_fetch_array($query23))
+                                {
+                                    $email1 = $row['email'];
+                                   // echo $email1;
+                                    $codeword1 = $row['codeword'];
+                                   // if($codeword1 != ""){   
+                                       // echo "checking ";    
+                                        $inc = rand ( 0 , count($temp)) ;  
+                                  //  $check[] = 
+
+                                     
+                                        $query24 = "UPDATE ".$course_code." SET codeword ='".$temp[$inc]."' where email='$email1'";
+                                        $query25 = mysqli_query($db, $query24) or die('error querying db');
+                                        //$inc = $inc + 1;
+                                    //}//end of if statement to check for codeword
+
+
+                                }
+                                // end of while loop for students
+
+
+                                include 'instructor_dashboard.php';
+
+
+                            }else{
+
+                                echo "<script>alert('Need more codewords to publish');</script>";
+                                include 'instructor_dashboard.php';
+
+                            }//end of nested-if-else statement
+                            
+                           //end of if statement
+
                         }else{
 
                             echo "<script>alert('You need to select codewordset to publish course');</script>";
